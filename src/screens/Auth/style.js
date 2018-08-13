0f9e0a6837361afa17f43b8f081ebc2f1a26d1a1
Jimmy Brannon <user.email>
2018-08-13 17:01:27 -0500

    resolve merge conflict

--- src/screens/Auth/style.js
+++ src/screens/Auth/style.js
@@ -3,11 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
     backgroundColor: '#fff',
-=======
-    backgroundColor: '#eee',
->>>>>>> navigation
     alignItems: 'center',
     justifyContent: 'center',
   },
