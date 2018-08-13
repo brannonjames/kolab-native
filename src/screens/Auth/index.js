e43da3d4ab34e02bf80389eee99f78bc844ff714
Jimmy Brannon <user.email>
2018-08-13 16:58:48 -0500

    break out auth styles

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from './style';
 
 export default class AuthScreen extends Component {
   render() {
@@ -10,12 +11,3 @@ export default class AuthScreen extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
