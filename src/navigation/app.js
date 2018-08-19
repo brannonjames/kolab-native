dfd57a86766e144689ef939ed562c1b69480ac4d
Jimmy Brannon <user.email>
2018-08-19 12:55:24 -0500

    show tabbar icons

--- src/navigation/app.js
+++ src/navigation/app.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import AuthScreen from '../screens/Auth';
 import SwipeNavigation from './nav_screens/swipe';
 import ProjectsNavigation from './nav_screens/projects';
 import AccountNavigation from './nav_screens/account';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import ChatScreen from '../screens/Chat';
 
@@ -21,7 +24,26 @@ const appTabs = createBottomTabNavigator({
     navigationOptions: { title: 'Account' }
   }
 }, {
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      switch(routeName) {
+        case 'swipe':
+          iconName = `ios-browsers${focused ? '' : '-outline'}`;
+          break;
+        case 'projects':
+          iconName = `ios-chatboxes${focused ? '' : '-outline'}`;
+          break;
+        case 'account':
+          iconName = `ios-apps${focused ? '' : '-outline'}`; 
+          break; 
+      }
+      return <Ionicons name={iconName} size={26} color={tintColor} />;
+    }
+  }),
   tabBarOptions: {
+    activeTintColor: '#8380B6',
     activeBackgroundColor: '#f4f8ff',
     inactiveBackgroundColor: '#f4f8ff'
   }
